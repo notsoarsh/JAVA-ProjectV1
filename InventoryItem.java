65730e614842e3af995c8382b91db93cc6fee305
notsoarsh <arshpalwork@gmail.com>
2025-04-26 14:44:25 +0530

    InventoryItem class was added.

---
+++ InventoryItem.java
@@ -0,0 +1,82 @@
+public class InventoryItem {
+    // Attributes of the item
+    private String itemId;
+    private String name;
+    private String category;
+    private int quantity;
+    private double price;
+    private String supplier;
+
+    
+    public InventoryItem(String itemId, String name, String category, int quantity, double price, String supplier) {
+        this.itemId = itemId;
+        this.name = name;
+        this.category = category;
+        this.quantity = quantity;
+        this.price = price;
+        this.supplier = supplier;
+    }
+
+    // Getter and Setter methods for each attribute
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(String supplier) {
+        this.supplier = supplier;
+    }
+
+    // Method to return a string representation of the object (toString)
+    @Override
+    public String toString() {
+        return "InventoryItem{" +
+                "itemId='" + itemId + '\'' +
+                ", name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", quantity=" + quantity +
+                ", price=" + price +
+                ", supplier='" + supplier + '\'' +
+                '}';
+    }
+
+}
